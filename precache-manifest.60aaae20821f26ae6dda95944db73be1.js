a2fdb3252f9c5783ed54f960030fd42856406fe0
KubaWysocki <jakub.wysocki.it@gmail.com>
2020-06-05 19:53:59 +0200

    Updates

---
+++ precache-manifest.60aaae20821f26ae6dda95944db73be1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10b4b7cc98cb1ddfd7a8975414c13f3e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ac19beec975b18a785ac",
+    "url": "./static/css/main.0365def1.chunk.css"
+  },
+  {
+    "revision": "992951808b16d7a24cd8",
+    "url": "./static/js/2.d8f2641f.chunk.js"
+  },
+  {
+    "revision": "d891fcc30dfc707e593ab65e09f6fef9",
+    "url": "./static/js/2.d8f2641f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac19beec975b18a785ac",
+    "url": "./static/js/main.180ba18c.chunk.js"
+  },
+  {
+    "revision": "8cc58b5042b0bd5a8388",
+    "url": "./static/js/runtime-main.edcd2301.js"
+  },
+  {
+    "revision": "d76076b20b676c08867bc1362676ebd3",
+    "url": "./static/media/FRAME.d76076b2.png"
+  },
+  {
+    "revision": "fbe31c4a5baabe7a48ba68609526d47e",
+    "url": "./static/media/arrow.fbe31c4a.png"
+  },
+  {
+    "revision": "266d8a41bf9c41e284adec1b1bd2d0ec",
+    "url": "./static/media/snake.266d8a41.png"
+  }
+]);
